10a247caa285e855d3977bcffaf2a752e3306027
Deep Patel <pdeep1933@gmail.com>
2017-12-05 11:39:42 -0600

    Angular 4 -- basic assignment with default data which you can update it.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+	{ id : 1, name: 'Deep' },
+	{ id : 2, name: 'Jay' },
+	{ id : 3, name: 'Aakanksha' },
+	{ id : 4, name: 'Prachi' },
+	{ id : 5, name: 'Somya'  },
+	{ id : 6, name: 'Kinsee' },
+	{ id : 7, name: 'Yash'  },
+	{ id : 8, name: 'Mitesh' },
+	{ id : 9, name: 'Aria'  },
+	{ id : 10, name: 'Krunal' },
+];
